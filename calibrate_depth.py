f3b80b9a895d39d170e814086925314dea4d0403
Jiuxun Yin <16894853+yinjiuxun@users.noreply.github.com>
2022-06-29 11:25:04 -0700

    Considered the different between hypocenter and epicenter distance

--- calibrate_depth.py
+++ calibrate_depth.py
@@ -1,9 +1,29 @@
 #%% import modules
 import os
+from cv2 import normalize
 import pandas as pd
 #from sep_util import read_file
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Plotting
+import matplotlib
+# %matplotlib inline
+params = { 
+    'image.interpolation': 'nearest',
+    'image.cmap': 'gray',
+    'savefig.dpi': 300,  # to adjust notebook inline plot size
+    'axes.labelsize': 18, # fontsize for x and y labels (was 10)
+    'axes.titlesize': 18,
+    'font.size': 18,
+    'legend.fontsize': 18,
+    'xtick.labelsize': 18,
+    'ytick.labelsize': 18,
+    'text.usetex':False,
+    'axes.facecolor': 'white',
+    'savefig.facecolor': 'white'
+}
+matplotlib.rcParams.update(params)
 
 #%%
 das_pick_file_folder_list = ['/kuafu/yinjx/Ridgecrest/Ridgecrest_scaling/peak_amplitude_events',
@@ -16,6 +36,7 @@ catalog_file_list = ['/kuafu/EventData/Ridgecrest/catalog.csv',
                 '/kuafu/EventData/Mammoth_north/catalog.csv',
                 '/kuafu/EventData/Sanriku_ERI/catalog.csv']
 
+region_list = ['Ridgecrest', 'Long-Vally south', 'Long-Valley north', 'Sanriku']
 
 das_pick_file_name = 'peak_amplitude.csv'
 
@@ -46,7 +67,7 @@ for ii in [3]:#range(4):
     peak_amplitude_df.to_csv(das_pick_file_folder + '/calibrated_' + das_pick_file_name, index=False)
 
 # %%
-fig, ax = plt.subplots(2,2, figsize=(8, 8))
+fig, ax = plt.subplots(2,2, figsize=(12, 12), sharex=True, sharey=True)
 ax = ax.flatten()
 for ii in range(4):
     das_pick_file_folder = das_pick_file_folder_list[ii]
@@ -57,6 +78,12 @@ for ii in range(4):
     catalog = pd.read_csv(catalog_file)
 
     gca = ax[ii]
-    gca.hist(peak_amplitude_df.distance_in_km, alpha=0.5, range=(0, 300), bins=100)
-    gca.hist(peak_amplitude_df.calibrated_distance_in_km, alpha=0.5, range=(0, 300), bins=100)
+    gca.hist(np.log10(peak_amplitude_df.distance_in_km), alpha=0.5, range=(0, 3.5), bins=50, density=True, label='epicenter distance')
+    gca.hist(np.log10(peak_amplitude_df.calibrated_distance_in_km), alpha=0.5, range=(0, 3.5), bins=50, density=True, label='hypocenter distance')
+    gca.set_ylabel('PDF')
+    gca.set_xlabel('distance (km) in log10')
+    gca.set_title(region_list[ii])
+
+gca.legend()
+plt.savefig('depth_calibrated_distance_distribution.png', bbox_inches='tight')
 # %%
