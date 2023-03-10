d54c43224d69a3f7d98f56ef34fb1eb50d239f0b
Jiuxun Yin <16894853+yinjiuxun@users.noreply.github.com>
2023-03-10 15:26:59 -0800

    update and simplify the regression code

--- regression/iter_regression.py
+++ regression/iter_regression.py
@@ -1,8 +1,6 @@
 # %%
 # import modules
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import sys
 sys.path.append('../')
 from utility.general import mkdir
@@ -10,7 +8,7 @@ from utility.processing import remove_outliers
 from utility.regression import store_regression_results, fit_regression_iteration
 
 def main(data_file, results_output_dir, weighted='wls', wavetypes=['P', 'S'], 
-         snr_threshold=10, min_channel=100, n_iter=20, rms_epsilon=0.1):
+         M_threshold=[2, 10], snr_threshold=10, min_channel=100, n_iter=20, rms_epsilon=0.1):
     
     # Set parameters
     outlier_value = 1e4
@@ -21,24 +19,31 @@ def main(data_file, results_output_dir, weighted='wls', wavetypes=['P', 'S'],
     # Load data
     peak_amplitude_dataframe = load_peak_amplitude_data(data_file, outlier_value)
 
+    ##########################
+    if peak_amplitude_dataframe['region'].unique()[0] == 'sanriku': # some special processing for Sanriku data
+        peak_amplitude_dataframe = peak_amplitude_dataframe.drop(index=peak_amplitude_dataframe[peak_amplitude_dataframe.event_id == 4130].index)
+    ##########################
+
     # Fit regressions
     regression_results = {}
     site_terms_dataframe = pd.DataFrame(columns=['region', 'channel_id', 'site_term_P', 'site_term_S'])
     for wavetype in wavetypes:
         try:
             regression, site_terms = fit_regression_iteration(peak_amplitude_dataframe, wavetype=wavetype, weighted=weighted,
-                                                               M_threshold=None, snr_threshold=snr_threshold,
+                                                               M_threshold=M_threshold, snr_threshold=snr_threshold,
                                                                min_channel=min_channel, n_iter=n_iter, rms_epsilon=rms_epsilon)
             regression_results[wavetype] = regression
+            # Store results
+            store_regression_results(regression_results[wavetype], results_output_dir, results_filename=f"/{wavetype}_regression_combined_site_terms_iter")
+            
             site_terms['wavetype'] = wavetype
             # site_terms_dataframe = site_terms_dataframe.append(site_terms, ignore_index=True)
             site_terms_dataframe = pd.concat((site_terms_dataframe, site_terms), ignore_index=True)
+
         except Exception as e:
             print(f"{wavetype} regression failed with error: {e}")
 
-    # Store results
-    store_regression_results(regression_results['P'], results_output_dir, results_filename="/P_regression_combined_site_terms_iter")
-    store_regression_results(regression_results['S'], results_output_dir, results_filename="/S_regression_combined_site_terms_iter")
+    # Store site terms
     site_terms_dataframe.to_csv(f"{results_output_dir}/site_terms_iter.csv", index=False)
 
 
@@ -58,182 +63,43 @@ def load_peak_amplitude_data(data_file, outlier_value):
     peak_amplitude_dataframe = peak_amplitude_dataframe.drop(index=peak_amplitude_dataframe[peak_amplitude_dataframe.peak_P <= 0].index)
     peak_amplitude_dataframe = peak_amplitude_dataframe.drop(index=peak_amplitude_dataframe[peak_amplitude_dataframe.peak_S <= 0].index)
 
+    # A special case when there is other quality control QA
+    if 'QA' in peak_amplitude_dataframe.columns:
+        peak_amplitude_dataframe = peak_amplitude_dataframe[peak_amplitude_dataframe.QA == 'Yes']
+
     return peak_amplitude_dataframe
 
-if __name__ == '__main__':
+if __name__ == '__main__':   
+    # First apply to the combined dataset of the three CA array
     # setup the directory of data and results
     data_file = '../data_files/peak_amplitude/peak_amplitude_multiple_arrays.csv'
     results_output_dir = '../iter_results'
     mkdir(results_output_dir)
-
     # run the iterative regression
     main(data_file, results_output_dir)
 
-# # %%
-# #%% import modules
-# import pandas as pd
-# #from sep_util import read_file
-# import numpy as np
-# import matplotlib.pyplot as plt
+    # Next apply to the individual array
+    data_file_list = ['../data_files/peak_amplitude/peak_amplitude_Ridgecrest.csv',
+                      '../data_files/peak_amplitude/peak_amplitude_LongValley_N.csv',
+                      '../data_files/peak_amplitude/peak_amplitude_LongValley_S.csv',
+                      '../data_files/peak_amplitude/peak_amplitude_Sanriku.csv']  
 
-# # import utility functions
-# import sys
-# sys.path.append('../')
-# from utility.general import mkdir
-# from utility.processing import combined_regions_for_regression, filter_event, remove_outliers
-# from utility.regression import store_regression_results, fit_regression_iteration
-
-# #%% 
-# # some parameters
-# snr_threshold = 10
-# min_channel = 100 # do regression only on events recorded by at least 100 channels
-
-# # parameters for the iterative regression
-# n_iter = 20
-# rms_epsilon = 0.1 # percentage of rms improvement, if smaller, stop iteration
-
-# # data path
-# data_dir = '../data_files/peak_amplitude'
-
-# # result directory
-# results_output_dir = '../results'
-# mkdir(results_output_dir)
-
-# #%% Iteratively fitting
-# weighted = 'wls' # 'ols' or 'wls'
-# if weighted == 'ols':
-#     weight_text = '' 
-# elif weighted == 'wls':
-#     weight_text = '_weighted' 
-# else:
-#     raise
-
-# regression_results_dir = results_output_dir + f'/iter_regression_results_smf{weight_text}_{min_channel}_channel_at_least_std'
-# mkdir(regression_results_dir)
-
-# peak_amplitude_df0 = pd.read_csv(data_dir + '/peak_amplitude_multiple_arrays.csv')
-# peak_amplitude_df0['distance_in_km'] = peak_amplitude_df0['calibrated_distance_in_km']
-
-# peak_amplitude_df0 = peak_amplitude_df0[['event_id', 'magnitude', 'depth_km', 'channel_id', 'distance_in_km', 
-#                                     'snrP', 'snrS', 'peak_P', 'peak_S', 'region']] 
-
-# # to remove some extreme values
-# peak_amplitude_df0 = remove_outliers(peak_amplitude_df0, outlier_value=1e4)
-# peak_amplitude_df0 = peak_amplitude_df0.drop(index=peak_amplitude_df0[peak_amplitude_df0.peak_P<=0].index)
-# peak_amplitude_df0 = peak_amplitude_df0.drop(index=peak_amplitude_df0[peak_amplitude_df0.peak_S<=0].index)
-
-
-
-# try:
-#     regP, site_term_df_P = fit_regression_iteration(peak_amplitude_df0, wavetype='P', weighted=weighted, 
-#                                 M_threshold=None, snr_threshold=snr_threshold, min_channel=min_channel, 
-#                                 n_iter=n_iter, rms_epsilon=rms_epsilon)
-# except:
-#     print('P regression is unavailable, assign Nan and None')
-#     regP = None
-#     site_term_df_P = pd.DataFrame(columns=['region', 'channel_id', 'site_term_P'])
-
-# try:
-#     regS, site_term_df_S = fit_regression_iteration(peak_amplitude_df0, wavetype='S', weighted=weighted, 
-#                                 M_threshold=None, snr_threshold=snr_threshold, min_channel=min_channel, 
-#                                 n_iter=n_iter, rms_epsilon=rms_epsilon)
-# except:
-#     print('S regression is unavailable, assign Nan and None')
-#     regS = None
-#     site_term_df_S = pd.DataFrame(columns=['region', 'channel_id', 'site_term_S'])
-
-# # merge the site term
-# site_term_df = pd.merge(site_term_df_P, site_term_df_S, how='outer', left_on=['channel_id', 'region'], right_on=['channel_id', 'region'])
-
-# # store the regression results
-# results_file_name = "regression_combined_site_terms_iter"
-# store_regression_results(regP, regression_results_dir, results_filename=f"/P_{results_file_name}")
-# store_regression_results(regS, regression_results_dir, results_filename=f"/S_{results_file_name}")
-# site_term_df.to_csv(regression_results_dir + f'/site_terms_iter.csv', index=False)
-
-
-
-
-# #%% 
-# # Apply to single array
-# region_list = ['ridgecrest', 'mammothS', 'mammothN', 'sanriku']
-# results_output_dirs = ["/OUTPUT/PATH/TO/Ridgecrest",
-#                        "/OUTPUT/PATH/TO/LV_S",
-#                        "/OUTPUT/PATH/TO/LV_N", 
-#                        "/OUTPUT/PATH/TO/Sanriku"]
-
-# M_threshold_list = [[2, 10], [2, 10], [2, 10], [2, 10]]
-# snr_threshold_list = [10, 10, 10, 5]
-# min_channel = 100
-
-# weighted = 'wls' # 'ols' or 'wls'
-# if weighted == 'ols':
-#     weight_text = '' 
-# elif weighted == 'wls':
-#     weight_text = '_weighted' 
-# else:
-#     raise
-
-# for i_region in range(len(results_output_dirs)):
-#     M_threshold = M_threshold_list[i_region]
-#     snr_threshold = snr_threshold_list[i_region]
-#     results_output_dir = results_output_dirs[i_region]
-#     print(results_output_dir)
-
-#     regression_results_dir = results_output_dir + f'/iter_regression_results_smf{weight_text}_{min_channel}_channel_at_least'
-#     mkdir(regression_results_dir)
-
-#     peak_amplitude_df = peak_amplitude_df0[peak_amplitude_df0.region == region_list[i_region]]
-
-#     if region_list[i_region] == 'sanriku': # some special processing for Sanriku data
-#         peak_amplitude_df = peak_amplitude_df[peak_amplitude_df.QA == 'Yes']
-#         peak_amplitude_df = peak_amplitude_df.drop(index=peak_amplitude_df[peak_amplitude_df.event_id == 4130].index)
-#         #peak_amplitude_df = peak_amplitude_df.drop(index=peak_amplitude_df[peak_amplitude_df.event_id == 1580].index)
-
-#     peak_amplitude_df = peak_amplitude_df[['event_id', 'magnitude', 'depth_km', 'channel_id', 'distance_in_km', 
-#                                         'snrP', 'snrS', 'peak_P', 'peak_S', 'region']] 
-                                    
-#     # to remove some extreme values
-#     peak_amplitude_df = remove_outliers(peak_amplitude_df, outlier_value=1e4)
-#     peak_amplitude_df = peak_amplitude_df.drop(index=peak_amplitude_df[peak_amplitude_df.peak_P<=0].index)
-#     peak_amplitude_df = peak_amplitude_df.drop(index=peak_amplitude_df[peak_amplitude_df.peak_S<=0].index)
-
-#     # Remove some bad data (clipped, poor-quality)
-#     if region_list[i_region] == 'ridgecrest':
-#         peak_amplitude_df = peak_amplitude_df.drop(index=peak_amplitude_df[peak_amplitude_df.event_id.isin([38548295.0, 39462536.0])].index)
-#     if region_list[i_region] == 'mammothN':
-#         peak_amplitude_df = peak_amplitude_df.drop(index=peak_amplitude_df[peak_amplitude_df.event_id == 73584926].index)
-#     if region_list[i_region] == 'mammothS':
-#         peak_amplitude_df = peak_amplitude_df.drop(index=peak_amplitude_df[peak_amplitude_df.event_id == 73584926].index)
-
-#     n_iter = 50
-#     rms_epsilon = 0.1 # percentage of rms improvement, if smaller, stop iteration
-#     show_figure = True
+    results_output_dir_list = ['../iter_results_Ridgecrest',
+                               '../iter_results_LongValley_N',
+                               '../iter_results_LongValley_S',
+                               '../iter_results_Sanriku']  
     
-#     try:
-#         regP, site_term_df_P = fit_regression_iteration(peak_amplitude_df, wavetype='P', weighted=weighted, 
-#                                     M_threshold=M_threshold, snr_threshold=snr_threshold, min_channel=min_channel, 
-#                                     n_iter=n_iter, rms_epsilon=rms_epsilon, show_figure=show_figure)
-#     except:
-#         print('P regression is unavailable, assign Nan and None')
-#         regP = None
-#         site_term_df_P = pd.DataFrame(columns=['region', 'channel_id', 'site_term_P', 'site_term_P_STD'])
+    # setup the directory of data and results
+    for i_region in range(len(data_file_list)):
+        data_file = data_file_list[i_region]
+        results_output_dir = results_output_dir_list[i_region]
+        mkdir(results_output_dir)
 
-#     try:
-#         regS, site_term_df_S = fit_regression_iteration(peak_amplitude_df, wavetype='S', weighted=weighted, 
-#                                     M_threshold=M_threshold, snr_threshold=snr_threshold, min_channel=min_channel, 
-#                                     n_iter=n_iter, rms_epsilon=rms_epsilon, show_figure=show_figure)
-#     except:
-#         print('S regression is unavailable, assign Nan and None')
-#         regS = None
-#         site_term_df_S = pd.DataFrame(columns=['region', 'channel_id', 'site_term_S', 'site_term_S_STD'])
+        if 'Sanriku' in data_file:
+            snr_threshold = 5 # for Sanriku, we loose the SNR requirement, or there will be too few events to get the scaling relation
+        else:
+            snr_threshold = 10
 
-#     # merge the site term
-#     site_term_df = pd.merge(site_term_df_P, site_term_df_S, how='outer', left_on=['channel_id', 'region'], right_on=['channel_id', 'region'])
-
-#     # store the regression results
-#     results_file_name = "regression_combined_site_terms_iter"
-#     store_regression_results(regP, regression_results_dir, results_filename=f"/P_{results_file_name}")
-#     store_regression_results(regS, regression_results_dir, results_filename=f"/S_{results_file_name}")
-#     site_term_df.to_csv(regression_results_dir + f'/site_terms_iter.csv', index=False)
-# %%
+        # run the iterative regression
+        print("Working on " + data_file)
+        main(data_file, results_output_dir, snr_threshold=snr_threshold, min_channel=100)
--- regression/transfer_regression.py
+++ regression/transfer_regression.py
@@ -1,16 +1,14 @@
 #%% import modules
-import os
 import pandas as pd
 import numpy as np
 import shutil
 import statsmodels.api as sm
 import random
-import matplotlib.pyplot as plt
 
 import sys
 sys.path.append('../')
 from utility.general import mkdir
-from utility.processing import filter_event
+from utility.processing import filter_event_first_order
 from utility.regression import fit_regression_transfer, get_std_of_site_terms
 
 #%% 
@@ -84,7 +82,7 @@ def main(data_file, previous_regression_dir, results_output_dir, N_event_fit_lis
 
     # peak_amplitude_df = pd.read_csv(results_output_dir + '/peak_amplitude_events/calibrated_peak_amplitude.csv')
     peak_amplitude_df['distance_in_km'] = peak_amplitude_df['calibrated_distance_in_km']
-    peak_amplitude_df = filter_event(peak_amplitude_df, snr_threshold=snr_threshold, min_channel=min_channel, M_threshold=M_threshold)
+    peak_amplitude_df = filter_event_first_order(peak_amplitude_df, snr_threshold=snr_threshold, min_channel=min_channel, M_threshold=M_threshold)
 
     # set seeds for randomly splitting dataset
     random.seed(212)
@@ -101,11 +99,8 @@ def main(data_file, previous_regression_dir, results_output_dir, N_event_fit_lis
             site_term_df = transfer_fitting(regP_pre, regS_pre, peak_amplitude_df_fit, weighted, M_threshold, snr_threshold, min_channel)
 
             # make output directory and output results
-            results_output_dir = results_output_dir
-            regression_results_dir = results_output_dir + f'/transfer_regression_test_smf{weight_text}_{min_channel}_channel_at_least'
-            mkdir(regression_results_dir)
-            
-            regression_results_dir = regression_results_dir + f'/{N_event_fit}_fit_events_{i_test}th_test'
+            results_output_dir = results_output_dir           
+            regression_results_dir = results_output_dir + f'/{N_event_fit}_fit_events_{i_test}th_test'
             mkdir(regression_results_dir)
 
             site_term_df.to_csv(regression_results_dir + '/site_terms_transfer.csv', index=False)
@@ -136,80 +131,4 @@ if __name__ == '__main__':
     # run the iterative regression
     main(data_file, previous_regression_dir, results_output_dir, N_event_fit_list, N_test)
 
-
-# #%%
-# # some parameters
-# min_channel = 100 # do regression only on events recorded by at least 100 channels
-
-# weighted = 'wls' # 'ols' or 'wls'
-# if weighted == 'ols':
-#     weight_text = '' 
-# elif weighted == 'wls':
-#     weight_text = '_weighted' 
-# else:
-#     raise NameError('weighted must be either "wls" for weighted regression or "old" for ordinary linear regression')
-# #%%
-# # Coefficients from previous results
-# previous_regression_dir = f'../results/'
-# regP_pre_path = previous_regression_dir + f"/P_regression_combined_site_terms_iter.pickle"
-# regS_pre_path = previous_regression_dir + f"/S_regression_combined_site_terms_iter.pickle"
-
-# regP_pre = sm.load(regP_pre_path)
-# regS_pre = sm.load(regS_pre_path)
-
-# #%%
-# snr_threshold_transfer = 5
-# M_threshold = [2, 10]
-
-# # setup the output directory
-# results_output_dir = '../transfered_results/'
-# mkdir(results_output_dir)
-
-# # load the data
-# data_file = '../data_files/peak_amplitude/peak_amplitude_Sanriku.csv'
-# with open(f"{data_file}", 'r') as f:
-#         peak_amplitude_df = pd.read_csv(f)
-
-# # peak_amplitude_df = pd.read_csv(results_output_dir + '/peak_amplitude_events/calibrated_peak_amplitude.csv')
-# peak_amplitude_df['distance_in_km'] = peak_amplitude_df['calibrated_distance_in_km']
-# peak_amplitude_df = filter_event(peak_amplitude_df, snr_threshold=snr_threshold_transfer, min_channel=min_channel, M_threshold=M_threshold)
-# event_id_all =  peak_amplitude_df.event_id.unique()
-
-# random.seed(212)
-# N_event_fit_list = [5]#range(2, 13)
-# N_test = 5
-
-# for N_event_fit in N_event_fit_list:
-#     for i_test in range(N_test):
-#         print(f'================= {N_event_fit} to fit, {i_test}th test =====================')
-
-#         # Randomly choose a few events to fit
-#         event_id_fit, peak_amplitude_df_fit, event_id_predict, peak_amplitude_df_predict = split_fit_and_predict(N_event_fit, peak_amplitude_df)
-#         event_id_fit_P0, event_id_fit_S0 = event_id_fit, event_id_fit
-                
-#         # Transfer scaling to obtain site terms
-#         site_term_df = transfer_fitting(regP_pre, regS_pre, peak_amplitude_df_fit, weighted)
-
-#         # make output directory and output results
-#         results_output_dir = results_output_dir
-#         regression_results_dir = results_output_dir + f'/transfer_regression_test_smf{weight_text}_{min_channel}_channel_at_least'
-#         mkdir(regression_results_dir)
-        
-#         regression_results_dir = regression_results_dir + f'/{N_event_fit}_fit_events_{i_test}th_test'
-#         mkdir(regression_results_dir)
-
-#         site_term_df.to_csv(regression_results_dir + '/site_terms_transfer.csv', index=False)
-
-#         # output the event id list of fit and predict events
-#         np.savez(regression_results_dir + '/transfer_event_list.npz', 
-#             event_id_fit_P=event_id_fit_P0, event_id_fit_S=event_id_fit_S0, event_id_predict=event_id_predict)
-
-#         # also copy the regression results to the results directory
-#         shutil.copyfile(regP_pre_path, regression_results_dir + '/P_regression_combined_site_terms_transfer.pickle')
-#         shutil.copyfile(regS_pre_path, regression_results_dir + '/S_regression_combined_site_terms_transfer.pickle')
-
-
-
-
-
-# # %%
+# %%
